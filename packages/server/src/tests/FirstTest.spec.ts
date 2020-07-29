2e3746153826a00ea65081716b74d1d139d843c0
pedroksty <pedrotrab2132@gmail.com>
2020-07-29 02:49:20 -0300

    Add Jest

--- packages/server/src/tests/FirstTest.spec.ts
+++
@@ -1,8 +0,0 @@
-test('it should be ok', () => {
-  const x = 2
-  const y = 3
-
-  const sumn = 5
-
-  expect(x + y.toEqual(5)
-})
---
+++ packages/web/web.spec.ts
@@ -0,0 +1,5 @@
+describe('Tester', () => {
+  it('should something', () => {
+    expect(1 + 4).toEqual(5)
+  })
+})
