e30d6dc063a485901af08798d0fd9c351fbd0570
pedroksty <pedrotrab2132@gmail.com>
2020-08-02 06:00:59 -0300

    Add React native cli

---
+++ packages/mobile/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>dale caralho</Text>
+    </View>
+  )
+}
+
+export default App
